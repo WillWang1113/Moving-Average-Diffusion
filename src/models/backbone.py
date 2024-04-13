eda05663b7579540842be5c12fcf5cd98cc76749
WillWang1113 <willwang1113@163.com>
2024-04-13 06:12:16 +0000

    add unet backbone, need debug

--- src/models/backbone.py
+++ src/models/backbone.py
@@ -1,36 +1,162 @@
 import torch
+import math
+from typing import Optional, List, Union, Tuple
 from torch import nn
 from torchvision.ops import MLP
-from .embedding import GaussianFourierProjection
+from .embedding import GaussianFourierProjection, SinusoidalPosEmb
+from .blocks import UpBlock, DownBlock, MiddleBlock, Downsample, Upsample
+
 
 class MLPBackbone(nn.Module):
     def __init__(
-        self, dim: int, timesteps: int, hidden_size: int, T: int, num_layers: int = 10
+        self, seq_channels: int, seq_length: int, hidden_size: int, num_layers: int = 10
     ) -> None:
         super(MLPBackbone, self).__init__()
-        self.embedder = nn.Linear(dim * timesteps, hidden_size)
-        self.unembedder = nn.Linear(hidden_size, dim * timesteps)
+        self.embedder = nn.Linear(seq_channels * seq_length, hidden_size)
+        self.unembedder = nn.Linear(hidden_size, seq_channels * seq_length)
         self.pe = GaussianFourierProjection(hidden_size)
         self.net = nn.ModuleList(  # type: ignore
             [
                 MLP(
                     in_channels=hidden_size,
                     hidden_channels=[1024, hidden_size],
-                    # dropout=0.1,
+                    dropout=0.1,
                 )
                 for _ in range(num_layers)
             ]
         )
+        self.channals = seq_channels
+        self.length = seq_length
 
-    def forward(self, x_noisy, t, conditions=None):
-        
-        x = self.embedder(x_noisy.flatten(1))
+    def forward(self, x, t, conditions=None):
+        x = self.embedder(x.flatten(1))
         x = self.pe(x, t, use_time_axis=False)
         for layer in self.net:
             x = x + layer(x)
-        x = self.unembedder(x).reshape(x_noisy.shape)
+        x = self.unembedder(x).reshape((-1, self.length, self.channals))
         return x
-    
-    
-# class CNNBackbone(nn.Module):
-    
+
+
+class UNetBackbone(nn.Module):
+    """
+    ## U-Net backbone
+    """
+
+    def __init__(
+        self,
+        seq_channels: int,
+        latent_channels: int = 64,
+        ch_mults: Union[Tuple[int, ...], List[int]] = (1, 2, 2, 4),
+        is_attn: Union[Tuple[bool, ...], List[bool]] = (False, False, True, True),
+        n_blocks: int = 2,
+    ):
+        """
+        * `seq_channels` is the number of channels in the time series. $1$ for uni-variable.
+        * `n_channels` is number of channels in the initial feature map that we transform the multi-var time series into
+        * `ch_mults` is the list of channel numbers at each resolution. The number of channels is `ch_mults[i] * latent_channels`
+        * `is_attn` is a list of booleans that indicate whether to use attention at each resolution
+        * `n_blocks` is the number of `UpDownBlocks` at each resolution
+        """
+        super().__init__()
+
+        # Number of resolutions
+        n_resolutions = len(ch_mults)
+
+        # Project time series into feature map
+        self.ts_proj = nn.Conv1d(seq_channels, latent_channels, kernel_size=3, padding=1)
+
+        # Time embedding layer. Time embedding has `n_channels * 4` channels
+        self.time_emb = SinusoidalPosEmb(latent_channels * 4)
+
+        # #### First half of U-Net - decreasing resolution
+        down = []
+        # Number of channels
+        out_channels = in_channels = latent_channels
+        # For each resolution
+        for i in range(n_resolutions):
+            # Number of output channels at this resolution
+            out_channels = in_channels * ch_mults[i]
+            # Add `n_blocks`
+            for _ in range(n_blocks):
+                down.append(
+                    DownBlock(in_channels, out_channels, latent_channels * 4, is_attn[i])
+                )
+                in_channels = out_channels
+            # Down sample at all resolutions except the last
+            if i < n_resolutions - 1:
+                down.append(Downsample(in_channels))
+
+        # Combine the set of modules
+        self.down = nn.ModuleList(down)
+
+        # Middle block
+        self.middle = MiddleBlock(
+            out_channels,
+            latent_channels * 4,
+        )
+
+        # #### Second half of U-Net - increasing resolution
+        up = []
+        # Number of channels
+        in_channels = out_channels
+        # For each resolution
+        for i in reversed(range(n_resolutions)):
+            # `n_blocks` at the same resolution
+            out_channels = in_channels
+            for _ in range(n_blocks):
+                up.append(
+                    UpBlock(in_channels, out_channels, latent_channels * 4, is_attn[i])
+                )
+            # Final block to reduce the number of channels
+            out_channels = in_channels // ch_mults[i]
+            up.append(UpBlock(in_channels, out_channels, latent_channels * 4, is_attn[i]))
+            in_channels = out_channels
+            # Up sample at all resolutions except last
+            if i > 0:
+                up.append(Upsample(in_channels))
+
+        # Combine the set of modules
+        self.up = nn.ModuleList(up)
+
+        # Final normalization and convolution layer
+        self.norm = nn.GroupNorm(8, latent_channels)
+        self.act = nn.SELU()
+        self.final = nn.Conv1d(in_channels, seq_channels, kernel_size=3, padding=1)
+
+    def forward(self, x: torch.Tensor, t: torch.Tensor):
+        """
+        * `x` has shape `[batch_size, seq, in_channels]`
+        * `t` has shape `[batch_size]`
+        """
+        # Transpose
+        x = x.permute(0, 2, 1)
+
+        # Get time-step embeddings
+        t = self.time_emb(t)
+
+        # Get image projection
+        x = self.ts_proj(x)
+
+        # `h` will store outputs at each resolution for skip connection
+        h = [x]
+        # First half of U-Net
+        for m in self.down:
+            x = m(x, t)
+            h.append(x)
+
+        # Middle (bottom)
+        x = self.middle(x, t)
+
+        # Second half of U-Net
+        for m in self.up:
+            if isinstance(m, Upsample):
+                x = m(x, t)
+            else:
+                # Get the skip connection from first half of U-Net and concatenate
+                s = h.pop()
+                x = torch.cat((x, s), dim=1)
+                #
+                x = m(x, t)
+
+        # Final normalization and convolution
+        return self.final(self.act(self.norm(x)))
--- tests/test_generative.py
+++ tests/test_generative.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
-local_name = Path(__file__).name.split('.')[0]
+local_name = Path(__file__).name.split(".")[0]
 
 T = 1000
 min_beta = 1e-4
@@ -17,40 +17,46 @@ epochs = 1000
 train_dl, test_dl = syntheic_sine()
 
 batch = next(iter(train_dl))
-input_ts, input_dim = batch["observed_data"].shape[1], batch["observed_data"].shape[2]
+input_ts, seq_channels = (
+    batch["observed_data"].shape[1],
+    batch["observed_data"].shape[2],
+)
 output_ts = batch["tp_to_predict"].shape[1]
 tp_to_predict = batch["tp_to_predict"][0].squeeze()
 
 diff = diffusion.DDPM(T=T, min_beta=min_beta, max_beta=max_beta, device=device)
-bb = backbone.MLPBackbone(
-    dim=input_dim, timesteps=input_ts, hidden_size=hidden_size, T=T
-).to(device)
-print('\n')
+bb = backbone.UNetBackbone(seq_channels=seq_channels).to(device)
+
+# bb = backbone.MLPBackbone(
+#     dim=input_dim, timesteps=input_ts, hidden_size=hidden_size, T=T
+# ).to(device)
+print("\n")
 print("MODEL PARAM:")
 print(sum([torch.numel(p) for p in bb.parameters()]))
-print('\n')
+print("\n")
 trainer = Trainer(
     diffusion=diff,
     backbone=bb,
     optimizer=torch.optim.Adam(bb.parameters()),
     device=device,
     epochs=epochs,
-    train_loss_fn=torch.nn.MSELoss(reduction='mean'),
+    train_loss_fn=torch.nn.MSELoss(reduction="mean"),
 )
 trainer.train(train_dl)
 y_pred, y_real = trainer.test(train_dl, n_sample=10)
 print(y_pred.shape)
 print(y_real.shape)
 
-sample_pred = y_pred[0,:,0,:].cpu().numpy()
-sample_real = y_real[0,:,0].cpu().numpy()
+sample_pred = y_pred[0, :, 0, :].cpu().numpy()
+sample_real = y_real[0, :, 0].cpu().numpy()
 
-fig, ax = plt.subplots(2,2)
+fig, ax = plt.subplots(2, 2)
 ax = ax.flatten()
-ax[0].plot(sample_real, label='real')
+ax[0].plot(sample_real, label="real")
 ax[0].legend()
 for i in range(1, 4):
-    ax[i].plot(sample_pred[...,i], c='black', alpha = 0.75, label='generated')
+    ax[i].plot(sample_pred[..., i], c="black", alpha=0.75, label="generated")
     ax[i].legend()
+ax.set_title(f'Backbone: {bb._get_name()}')
 fig.tight_layout()
-fig.savefig(f'assets/{local_name}.png')
+fig.savefig(f"assets/{local_name}.png")
