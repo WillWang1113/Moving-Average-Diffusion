87298acb289bb0ed114df214104c5ba2cdd18499
WillWang1113 <willwang1113@163.com>
2025-01-25 10:32:48 +0800

    update

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 import setuptools
 
 setuptools.setup(
-    name='AnonymousPackage',
+    name='MATSD',
     packages=setuptools.find_packages(),
     version='0.0.0',
-    author='Anonymous Authors',
 )
